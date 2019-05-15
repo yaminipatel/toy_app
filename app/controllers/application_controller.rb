126a36e65bfc202bd54eb564c40ce694573a9d58
EC2 Default User <yamini.patel41186@yahoo.com>
2019-05-15 02:54:47 +0000

    changed controllers file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    render html: "hello, world!"
+    render html: "hello, world! welcome to the real world"
   end
 end
